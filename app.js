861584aafd1707d736084440422fbba41cf03988
mike.zhang <mike.zhang@yeamoney.cn>
2017-11-17 15:51:00 +0800

    304
    最简单的静态文件返回304，接下来是要强制浏览器使用本地缓存（cache-control/expires），不要和服务器通信。

---
+++ app.js
@@ -0,0 +1,15 @@
+var express = require('express')
+var app = express()
+
+// respond with "hello world" when a GET request is made to the homepage
+app.get('/', function (req, res) {
+  res.sendFile(__dirname+'/index.html')
+})
+
+app.use('/css',express.static(__dirname+'/css'));
+
+var server = app.listen(9000,function(){    /*监听端口*/
+    var host = server.address().address;
+    var port = server.address().port; 
+    console.log('example app listening at http://%s:%s',host,port);  /*控制台输出*/
+});
