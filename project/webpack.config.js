6692768eae214ccee23ba764b252d038a46d31eb
zhanghang <mike.zhang@yeamoney.cn>
2017-11-18 17:40:08 +0800

    结合react实现代码分离与自动生成html
    但是 代码分离似乎没有成功  先放一放

--- project/webpack.config.js
+++ project/webpack.config.js
@@ -1,12 +1,49 @@
 var webpack = require('webpack');
 var path = require('path');
+var CleanWebpackPlugin = require('clean-webpack-plugin');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
+
+const VENDOR = ["faker",
+  "lodash",
+  "react",
+  "react-dom",
+  "react-input-range",
+  "react-redux",
+  "redux",
+  "redux-form",
+  "redux-thunk"
+];
 
 module.exports = {
-  entry: './src/index.js',
+  entry: {bundle: './src/index.js',vender: VENDOR},
   output: {
     path: path.join(__dirname, 'dist'),
-    filename: 'bundle.js'
+    // 既然我们希望缓存生效，就应该每次在更改代码以后修改文件名
+    // [chunkhash]会自动根据文件是否更改而更换哈希
+    filename: '[name].[chunkhash].js'
   },
+  plugins: [
+    new webpack.optimize.CommonsChunkPlugin({
+    // vendor 的意义和之前相同
+    // manifest文件是将每次打包都会更改的东西单独提取出来，保证没有更改的代码无需重新打包，这样可以加快打包速度
+      names: ['vendor', 'manifest'],
+      // 配合 manifest 文件使用
+      minChunks: Infinity
+    }),
+    // 只删除 dist 文件夹下的 bundle 和 manifest 文件
+    new CleanWebpackPlugin(['dist/bundle.*.js','dist/manifest.*.js'], {
+    // 打印 log
+      verbose: true,
+      // 删除文件
+      dry: false
+    }),
+    // 我们这里将之前的 HTML 文件当做模板
+  // 注意在之前 HTML 文件中请务必删除之前引入的 JS 文件
+    new HtmlWebpackPlugin({
+      template: 'index.html'
+    })
+  ],
   module: {
     rules:[
       {
