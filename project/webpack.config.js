8c39574c3639901dd3b02fd1d118f575186ac0be
zhanghang <mike.zhang@yeamoney.cn>
2017-11-18 17:59:29 +0800

    完成生成环境的配置，结合所有以前的内容

--- project/webpack.config.js
+++ project/webpack.config.js
@@ -1,10 +1,11 @@
 var webpack = require('webpack');
 var path = require('path');
-var CleanWebpackPlugin = require('clean-webpack-plugin');
-var HtmlWebpackPlugin = require('html-webpack-plugin');
+var HtmlWebpackPlugin = require('html-webpack-plugin')
+var CleanWebpackPlugin = require('clean-webpack-plugin')
+var ExtractTextPlugin = require('extract-text-webpack-plugin')
+var OptimizeCSSPlugin = require('optimize-css-assets-webpack-plugin')
 
-
-const VENDOR = ["faker",
+const VENOR = ["faker",
   "lodash",
   "react",
   "react-dom",
@@ -14,47 +15,77 @@ const VENDOR = ["faker",
   "redux-form",
   "redux-thunk",
   "react-router"
-];
+]
 
 module.exports = {
-  entry: {vendor: VENDOR,bundle: './src/index.js'},
+  entry: {
+    bundle: './src/index.js',
+    vendor: VENOR
+  },
+  // 如果想修改 webpack-dev-server 配置，在这个对象里面修改
+  devServer: {
+    port: 8081
+  },
   output: {
     path: path.join(__dirname, 'dist'),
-    // 既然我们希望缓存生效，就应该每次在更改代码以后修改文件名
-    // [chunkhash]会自动根据文件是否更改而更换哈希
     filename: '[name].[chunkhash].js'
   },
-  plugins: [
-    new webpack.optimize.CommonsChunkPlugin({
-    // vendor 的意义和之前相同
-    // manifest文件是将每次打包都会更改的东西单独提取出来，保证没有更改的代码无需重新打包，这样可以加快打包速度
-      names: ['vendor', 'manifest'],
-      // 配合 manifest 文件使用
-      minChunks: Infinity
-    }),
-    // 只删除 dist 文件夹下的 bundle 和 manifest 文件
-    new CleanWebpackPlugin(['dist/bundle.*.js','dist/manifest.*.js'], {
-    // 打印 log
-      verbose: true,
-      // 删除文件
-      dry: false
-    }),
-    // 我们这里将之前的 HTML 文件当做模板
-  // 注意在之前 HTML 文件中请务必删除之前引入的 JS 文件
-    new HtmlWebpackPlugin({
-      template: 'index.html'
-    })
-  ],
   module: {
-    rules:[
-      {
+    rules: [{
         test: /\.js$/,
         use: 'babel-loader'
       },
       {
+        test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
+        use: [{
+            loader: 'url-loader',
+            options: {
+                limit: 10000,
+                name: 'images/[name].[hash:7].[ext]'
+            }
+        }]
+    },
+    {
         test: /\.css$/,
-        use: ['style-loader', 'css-loader']
-      }
+        loader: ExtractTextPlugin.extract({
+            fallback: 'style-loader',
+            use: [{
+            // 这边其实还可以使用 postcss 先处理下 CSS 代码
+                loader: 'css-loader'
+            }]
+        })
+    },
     ]
-  }
+  },
+  plugins: [
+    new webpack.optimize.CommonsChunkPlugin({
+      name: ['vendor', 'manifest'],
+      minChunks: Infinity
+    }),
+    new CleanWebpackPlugin(['dist/*.js'], {
+      verbose: true,
+      dry: false
+    }),
+    new HtmlWebpackPlugin({
+      template: 'index.html'
+    }),
+    // 生成全局变量
+    new webpack.DefinePlugin({
+      "process.env.NODE_ENV": JSON.stringify("process.env.NODE_ENV")
+    }),
+    // 分离 CSS 代码
+    new ExtractTextPlugin("css/[name].[contenthash].css"),
+    // 压缩提取出的 CSS，并解决ExtractTextPlugin分离出的 JS 重复问题
+    new OptimizeCSSPlugin({
+      cssProcessorOptions: {
+        safe: true
+      }
+    }),
+    // 压缩 JS 代码
+    new webpack.optimize.UglifyJsPlugin({
+      compress: {
+        warnings: false
+      }
+    })
+  ]
 };
